fd747e089cf9c02a1d0e8ed6ae04bef6bfb3a288
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-19 22:18:25 +0100

    Creating Coordinate and Wire entites

---
+++ AdventOfCode2019/Entities/Coordinate.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode2019.Entities
+{
+    public class Coordinate
+    {
+        public readonly int x;
+        public readonly int y;
+        public Coordinate(int xPoisition, int yPosition)
+        {
+            x = xPoisition;
+            y = yPosition;
+        }
+    }
+}
---
+++ AdventOfCode2019/Entities/Wire.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace AdventOfCode2019.Entities
+{
+    public class Wire
+    {
+        public List<Coordinate> previousPositions;
+        public Coordinate currentPosition;
+    }
+}

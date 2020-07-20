b97518d86007a61fd67dd2eac7f2567af6762026
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-20 22:02:16 +0100

    Adds constructor

--- AdventOfCode2019/Entities/Wire.cs
+++ AdventOfCode2019/Entities/Wire.cs
@@ -6,5 +6,9 @@ namespace AdventOfCode2019.Entities
     {
         public List<Coordinate> previousPositions = new List<Coordinate>();
         public Coordinate currentPosition;
+        public Wire(int StartX, int StartY)
+        {
+            currentPosition = new Coordinate(StartX, StartY);
+        }
     }
 }
